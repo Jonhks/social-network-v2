a317c3c5530a95d9eae5d1b748dc87a302b062fc
Jonh <Jonh@MacBook-de-Jonathan.local>
2018-07-27 09:31:45 -0500

    Subiendo maqueta sin funcionalidad

---
+++ src/js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCp30xQ0PrTeET_cyvp3MTQ4sdPXHplJTk",
+    authDomain: "red-social-v2.firebaseapp.com",
+    databaseURL: "https://red-social-v2.firebaseio.com",
+    projectId: "red-social-v2",
+    storageBucket: "red-social-v2.appspot.com",
+    messagingSenderId: "239665734039"
+  };
+  firebase.initializeApp(config);
